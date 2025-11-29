b12e5401f8eae9c6a32652bf43dde6199564469c
Minh Hoang <22520464@gm.uit.edu.vn>
2025-11-29 14:55:09 +0700

    rebuild

--- src/SimplCommerce.WebHost/Migrations/20251129000000_AddLargeSeedData.cs
+++ src/SimplCommerce.WebHost/Migrations/20251129000000_AddLargeSeedData.cs
@@ -9,60 +9,58 @@ namespace SimplCommerce.WebHost.Migrations
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            // SQL to insert large data (approx 1GB)
-            // 20,000 rows * ~50KB per row = ~1GB
-            var sql = @"
-                SET IDENTITY_INSERT [Catalog_Product] ON;
-                
-                DECLARE @i INT = 100000;
-                DECLARE @max INT = 120000; -- 20,000 rows
-                DECLARE @desc NVARCHAR(MAX);
-                DECLARE @userId BIGINT;
-                
-                -- Get first user ID from database (fallback to 2 if none exists)
-                SELECT TOP 1 @userId = Id FROM [Core_User] ORDER BY Id;
-                IF @userId IS NULL SET @userId = 2;
-                
-                -- Generate ~50KB text (25000 chars * 2 bytes)
-                -- 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. ' is 55 chars
-                -- 55 * 455 ~= 25025 chars
-                SET @desc = REPLICATE(CAST(N'Lorem ipsum dolor sit amet, consectetur adipiscing elit. ' AS NVARCHAR(MAX)), 455); 
+            // Insert 2,000 rows for testing (reduced from 20,000 to avoid timeout)
+            // Each batch = 200 rows, total 10 batches (~100MB total)
+            // For full 1GB data, use azure-seed-data.sql script directly on Azure Portal
+            int batchSize = 200;
+            int totalRows = 2000;
+            int startId = 100000;
 
-                WHILE @i < @max
-                BEGIN
-                    INSERT INTO [Catalog_Product] (
-                        [Id], [BrandId], [CreatedById], [CreatedOn], [Description], [DisplayOrder], 
-                        [HasOptions], [IsAllowToOrder], [IsCallForPricing], [IsDeleted], [IsFeatured], 
-                        [IsPublished], [IsVisibleIndividually], [LatestUpdatedById], [LatestUpdatedOn], 
-                        [Name], [NormalizedName], [Price], [ReviewsCount], [Sku], [Slug], 
-                        [StockQuantity], [StockTrackingIsEnabled], [TaxClassId], [ThumbnailImageId], [VendorId]
-                    ) VALUES (
-                        @i, 
-                        NULL, -- BrandId (NULL - no brand)
-                        @userId, -- CreatedById (from database)
-                        GETDATE(), 
-                        @desc, 
-                        0, 
-                        0, 1, 0, 0, 1, 
-                        1, 1, @userId, GETDATE(), 
-                        N'Large Data Product ' + CAST(@i AS NVARCHAR(20)), 
-                        N'LARGE DATA PRODUCT ' + CAST(@i AS NVARCHAR(20)), 
-                        1000000, 
-                        0, 
-                        N'SKU-' + CAST(@i AS NVARCHAR(20)), 
-                        N'large-data-product-' + CAST(@i AS NVARCHAR(20)), 
-                        100, 
-                        1, 
-                        NULL, NULL, NULL
-                    );
+            for (int batch = 0; batch < (totalRows / batchSize); batch++)
+            {
+                int batchStart = startId + (batch * batchSize);
+                int batchEnd = batchStart + batchSize;
 
-                    SET @i = @i + 1;
-                END
+                var sql = $@"
+                    SET IDENTITY_INSERT [Catalog_Product] ON;
+                    
+                    DECLARE @i INT = {batchStart};
+                    DECLARE @max INT = {batchEnd};
+                    DECLARE @desc NVARCHAR(MAX);
+                    DECLARE @userId BIGINT;
+                    
+                    SELECT TOP 1 @userId = Id FROM [Core_User] ORDER BY Id;
+                    IF @userId IS NULL SET @userId = 2;
+                    
+                    -- Reduced size: ~5KB per row (was 50KB)
+                    SET @desc = REPLICATE(CAST(N'Lorem ipsum dolor sit amet, consectetur adipiscing elit. ' AS NVARCHAR(MAX)), 45); 
 
-                SET IDENTITY_INSERT [Catalog_Product] OFF;
-            ";
+                    WHILE @i < @max
+                    BEGIN
+                        INSERT INTO [Catalog_Product] (
+                            [Id], [BrandId], [CreatedById], [CreatedOn], [Description], [DisplayOrder], 
+                            [HasOptions], [IsAllowToOrder], [IsCallForPricing], [IsDeleted], [IsFeatured], 
+                            [IsPublished], [IsVisibleIndividually], [LatestUpdatedById], [LatestUpdatedOn], 
+                            [Name], [NormalizedName], [Price], [ReviewsCount], [Sku], [Slug], 
+                            [StockQuantity], [StockTrackingIsEnabled], [TaxClassId], [ThumbnailImageId], [VendorId]
+                        ) VALUES (
+                            @i, NULL, @userId, GETDATE(), @desc, 0, 
+                            0, 1, 0, 0, 1, 1, 1, @userId, GETDATE(), 
+                            N'Large Data Product ' + CAST(@i AS NVARCHAR(20)), 
+                            N'LARGE DATA PRODUCT ' + CAST(@i AS NVARCHAR(20)), 
+                            1000000, 0, 
+                            N'SKU-' + CAST(@i AS NVARCHAR(20)), 
+                            N'large-data-product-' + CAST(@i AS NVARCHAR(20)), 
+                            100, 1, NULL, NULL, NULL
+                        );
+                        SET @i = @i + 1;
+                    END
 
-            migrationBuilder.Sql(sql);
+                    SET IDENTITY_INSERT [Catalog_Product] OFF;
+                ";
+
+                migrationBuilder.Sql(sql);
+            }
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
